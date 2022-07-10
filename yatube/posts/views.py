0b7f9c6a572af3d181c70c908d89bd98c8c5dd57
Марат Мелкумян <melkumyanmarat9@gmail.com>
2022-07-10 14:17:28 +0300

    !

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -21,9 +21,9 @@ def index(request):
     page_obj = paginator.get_page(page_number)
     context = {
         'page_obj': page_obj,
-        "posts": posts,
+        'posts': posts,
     }
-    return render(request, "posts/index.html", context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
@@ -33,32 +33,33 @@ def group_posts(request, slug):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
-        "group": group,
-        "posts": posts,
+        'group': group,
+        'posts': posts,
         'page_obj': page_obj,
     }
-    return render(request, "posts/group_list.html", context)
+    return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     posts = author.posts.all()
-    posts_count = posts.count()
-    paginator = Paginator(posts, 10)
+    paginator = Paginator(posts, settings.FOR_PAGINATOR)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'author': author,
-        'posts_count': posts_count,
+        'posts': posts,
         'page_obj': page_obj,
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+    post = get_object_or_404(Post, id=post_id)
+    posts_count = Post.objects.filter(author=post.author).count()
     context = {
         'post': post,
+        'posts_count': posts_count,
     }
     return render(request, 'posts/post_detail.html', context)
 
